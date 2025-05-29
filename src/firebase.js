2e7e78de6adb025ad77fd4d04949e952079ca575
askeparoski <alskeparoski@gmail.com>
2025-05-29 13:07:47 +0200

    Security: Move Firebase config to environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -2,15 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAI-8CU6-9EGqu9X4TDQ9rYowrIPOOJqhw",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "elvare-a654b.firebaseapp.com",
   projectId: "elvare-a654b",
-  storageBucket: "elvare-a654b.firebasestorage.app",
-  messagingSenderId: "1015704662766",
-  appId: "1:1015704662766:web:b1edf6e7ae1efce036f9ff"
+  storageBucket: "elvare-a654b.appspot.com",
+  messagingSenderId: "1234567890",
+  appId: "1:1234567890:web:abcdef1234567890"
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export { db };
+export const db = getFirestore(app);
